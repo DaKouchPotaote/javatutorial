8fc25e28ed97360d11620ae1521bc79f0cb66bbe
DaKouchPotaote <dakouchpotatoe@gmail.com>
2016-12-02 21:24:15 -0600

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+//Episode #1: Hello World
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		
+		//PRINT AND PRINTLN
+		System.out.println("Hello World!");
+		System.out.print("Hello again");
+		
+		//ERR
+		System.err.println("ERROR");
+	}
+}
